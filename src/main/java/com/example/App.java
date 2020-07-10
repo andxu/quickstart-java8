7c9f6f1d1879a2dd6d67850e0701cc75284e7600
Andy Xu(devdiv) <andxu@microsoft.com>
2020-07-10 15:26:26 +0800

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,7 +11,7 @@ public final class App {
      * Says hello to the world.
      * @param args The arguments of the program. test
      */
-    public static void main(String[] args) {
+              public static void main(String[] args)   {
         System.out.println("Hello World2"); // a minior change
     }
 }
